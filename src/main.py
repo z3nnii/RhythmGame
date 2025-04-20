4878eec16801ce8dfb3c28be02b7380f43b3e4e3
Owner <comimask@gmail.com>
2025-04-19 23:22:36 -0500

    Relocated keys

--- src/main.py
+++ src/main.py
@@ -9,12 +9,13 @@ class Key():
         self.color1 = color1
         self.color2 = color2
         self.key = key
+        self.rect = pygame.Rect(self.x,self.y,50,20)
 
-key = [
-    Key(100,100,(255,0,0),(0,0,0),pygame.K_a),
-    Key(100,100,(255,0,0),(0,0,0),pygame.K_s),
-    Key(100,100,(255,0,0),(0,0,0),pygame.K_d),
-    Key(100,100,(255,0,0),(0,0,0),pygame.K_f),
+keys = [
+    Key(100,100,(255,0,0),(220,0,0),pygame.K_a),
+    Key(200,100,(0,255,0),(0,220,0),pygame.K_s),
+    Key(300,100,(0,0,255),(0,0,220),pygame.K_d),
+    Key(400,100,(255,255,0),(220,220,0),pygame.K_f),
 ]
 
 while True:
@@ -22,3 +23,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+
+    for key in keys:
+        pygame.draw.rect(screen,key.color1,key.rect)
+
+
+
+    pygame.display.update()
