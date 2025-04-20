2435b11388292ea1ddff67de5b386ec9f961aaff
Owner <comimask@gmail.com>
2025-04-19 23:14:42 -0500

    Added keys

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 import pygame
-import sys
 
-pygame.init()
 screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
 
 def load_key_images(normal_path, pressed_path):
     normal = pygame.image.load(normal_path).convert_alpha()
@@ -13,20 +10,22 @@ def load_key_images(normal_path, pressed_path):
     return normal, pressed
 
 class Key():
-    def __init__(self, x, y, image_normal, image_pressed, key_code):
-        self.image_normal = image_normal
-        self.image_pressed = image_pressed
-        self.image = image_normal
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.key_code = key_code
-        self.pressed = False
+    def __init__(self, x, y, color1, color2, key):
+        self.x = x
+        self.y = y
+        self.color1 = color1
+        self.color2 = color2
+        self.key = key
 
-    def update(self):
-        self.pressed = pygame.key.get_pressed()[self.key_code]
-        self.image = self.image_pressed if self.pressed else self.image_normal
+key = [
+    Key(100,100,(255,0,0),(0,0,0),pygame.K_a),
+    Key(100,100,(255,0,0),(0,0,0),pygame.K_s),
+    Key(100,100,(255,0,0),(0,0,0),pygame.K_d),
+    Key(100,100,(255,0,0),(0,0,0),pygame.K_f)
+]
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+
+
 
 # Load all images
 a_normal, a_pressed = load_key_images("assets/key_a.png", "assets/key_a_pressed.png")
@@ -34,26 +33,3 @@ s_normal, s_pressed = load_key_images("assets/key_s.png", "assets/key_s_pressed.
 j_normal, j_pressed = load_key_images("assets/key_j.png", "assets/key_j_pressed.png")
 k_normal, k_pressed = load_key_images("assets/key_k.png", "assets/key_k_pressed.png")
 
-# Create keys
-keys = [
-    Key(100, 100, a_normal, a_pressed, pygame.K_a),
-    Key(200, 100, s_normal, s_pressed, pygame.K_s),
-    Key(500, 100, j_normal, j_pressed, pygame.K_j),
-    Key(600, 100, k_normal, k_pressed, pygame.K_k),
-]
-
-# Game loop
-while True:
-    screen.fill((30, 30, 30))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    for key in keys:
-        key.update()
-        key.draw(screen)
-
-    pygame.display.update()
-    clock.tick(60)
