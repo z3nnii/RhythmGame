37a20ed5109725d18f7941b5b99d40ced5d4a73f
Owner <comimask@gmail.com>
2025-04-19 23:34:19 -0500

    Added music mixer.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import pygame
+from pygame import mixer
+
+mixer.init()
+mixer.music.load('')
 
 screen = pygame.display.set_mode((800, 600))
 
@@ -12,10 +16,10 @@ class Key():
         self.rect = pygame.Rect(self.x,self.y,50,20)
 
 keys = [
-    Key(100,100,(255,0,0),(220,0,0),pygame.K_a),
-    Key(200,100,(0,255,0),(0,220,0),pygame.K_s),
-    Key(300,100,(0,0,255),(0,0,220),pygame.K_d),
-    Key(400,100,(255,255,0),(220,220,0),pygame.K_f),
+    Key(100,500,(255,0,0),(220,0,0),pygame.K_a),
+    Key(200,500,(0,255,0),(0,220,0),pygame.K_s),
+    Key(300,500,(0,0,255),(0,0,220),pygame.K_d),
+    Key(400,500,(255,255,0),(220,220,0),pygame.K_f),
 ]
 
 while True:
@@ -24,8 +28,12 @@ while True:
             pygame.quit()
             quit()
 
+    k = pygame.key.get_pressed()
     for key in keys:
-        pygame.draw.rect(screen,key.color1,key.rect)
+        if k[key.key]:
+            pygame.draw.rect(screen,key.color1,key.rect)
+        if not k[key.key]:
+            pygame.draw.rect(screen,key.color2,key.rect)
 
 
 
